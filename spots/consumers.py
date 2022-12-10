a92cd38123828d6ee15193bc75a82012c2788e16
Sergei Banshchikov <s.o.banshchikov@yandex.ru>
2022-12-10 03:26:14 +0200

    Change channel dict

--- spots/consumers.py
+++ spots/consumers.py
@@ -1,5 +1,6 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
+import math
 
 all_channels = {}
 
@@ -32,14 +33,28 @@ class ChatConsumer(AsyncWebsocketConsumer):
                 self.spot_group_name, self.channel_name
             )
 
-            lon, lat = text_data_json['spot']
+            longitude = text_data_json['spot'][0]
+            lon_spot = math.floor(longitude * 10) / 10
+            latitude = text_data_json['spot'][1]
+            lat_spot = math.floor(latitude * 10) / 10
             # if self.CURRENT_ZOOM >= 18:
-            all_channels[(lon, lat)] = self.channel_name
-
-            self.spot_group_name = str(lon) + str(lat)
-            await self.channel_layer.group_add(
-                self.spot_group_name, self.channel_name
-            )
+            if all_channels.get((lon_spot, lat_spot)):
+                all_channels[(lon_spot, lat_spot)].append(
+                    {
+                        'longlat': (longitude, latitude),
+                        'groups': []
+                    }
+                )
+            else:
+                all_channels[(lon_spot, lat_spot)] = [{
+                    'longlat': (longitude, latitude),
+                    'groups': []
+                    }]
+            print(all_channels)
+            # self.spot_group_name = str(lon) + str(lat)
+            # await self.channel_layer.group_add(
+            #     self.spot_group_name, self.channel_name
+            # )
         else:
             message = text_data_json['message']
 
